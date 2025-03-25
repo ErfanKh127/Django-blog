b7266708b5a32d06017921d2ae7918fa9f79bf2a
ErfanKh127 <erfankhosrobeygi127@gmail.com>
2025-03-25 19:36:50 +0330

    make page for posts and ability to make new post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
-from . import views
+
+from .views import PostListView, about, PostDetailView, PostCreateView
 from django.urls import path
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('about/', about, name='blog-about'),
 ]
