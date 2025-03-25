3a8725b8f15b9d5aefec0b20b4fa03147bf11a8b
ErfanKh127 <erfankhosrobeygi127@gmail.com>
2025-03-25 20:08:15 +0330

    give ability to user to update own posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 
-from .views import PostListView, about, PostDetailView, PostCreateView
+from .views import PostListView, about, PostDetailView, PostCreateView, PostUpdateView
 from django.urls import path
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('about/', about, name='blog-about'),
 ]
