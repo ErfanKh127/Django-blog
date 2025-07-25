f69848606310842f4f61b252b86dcc73e28d2da7
ErfanKh127 <erfankhosrobeygi127@gmail.com>
2025-07-25 22:02:07 +0330

    added requierments of collectstatic

--- django_project/settings.py
+++ django_project/settings.py
@@ -15,6 +15,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
